203cff6b48f1642e0e1303f8089a3f2ac620e9ac
anxysz <lucknurfitriana17@gmail.com>
2023-11-26 14:17:35 +0700

    template awal

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+//Route::get('/', function () {
+//    return view('welcome', ['title' => 'Sistem Parkir']);
+//});
+
+Route::get('/', function (){
+    return view('home');
 });
